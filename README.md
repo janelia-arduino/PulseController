e1446471abfb277ed6d7fabed72d8e8b1b8c263d
Peter Polidoro <peterpolidoro@gmail.com>
2017-12-18 13:29:38 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,42 @@
+# PulseController
+
+Authors:
+
+    Peter Polidoro <polidorop@janelia.hhmi.org>
+
+License:
+
+    BSD
+
+## Ancestors
+
+[ModularServer](https://github.com/janelia-arduino/ModularServer)
+
+[ModularDeviceBase](https://github.com/janelia-arduino/ModularDeviceBase)
+
+[PowerSwitchController](https://github.com/janelia-arduino/PowerSwitchController)
+
+## Clients
+
+## Devices
+
+[modular_device_base](https://github.com/janelia-modular-devices/modular_device_base.git)
+
+[power_switch__controller](https://github.com/janelia-modular-devices/power_switch_controller.git)
+
+## API NAMES
+
+```json
+```
+
+## API GENERAL
+
+[API GENERAL](./api/)
+
+## More Detailed Modular Device Information
+
+[modular-devices](https://github.com/janelia-modular-devices/modular-devices)
+
+## Install Library Dependencies
+
+[arduino-libraries](https://github.com/janelia-arduino/arduino-libraries)
---
+++ src/PulseController.h
@@ -0,0 +1,51 @@
+// ----------------------------------------------------------------------------
+// PulseController.h
+//
+//
+// Authors:
+// Peter Polidoro polidorop@janelia.hhmi.org
+// ----------------------------------------------------------------------------
+#ifndef PULSE_CONTROLLER_H
+#define PULSE_CONTROLLER_H
+#include <Flash.h>
+#include <EEPROM.h>
+#include <Streaming.h>
+#include <ArduinoJson.h>
+#include <JsonSanitizer.h>
+#include <JsonStream.h>
+#include <Array.h>
+#include <Vector.h>
+#include <MemoryFree.h>
+#include <ConstantVariable.h>
+#include <SavedVariable.h>
+#include <Functor.h>
+#include <IndexedContainer.h>
+#include <FunctorCallbacks.h>
+#include <Watchdog.h>
+
+#include <ModularClient.h>
+
+#include <ModularServer.h>
+#include <ModularDeviceBase.h>
+#include <PowerSwitchController.h>
+
+#include "PulseController/Constants.h"
+
+
+class PulseController : public PowerSwitchController
+{
+public:
+  PulseController();
+  virtual void setup();
+
+private:
+  modular_server::Property properties_[pulse_controller::constants::PROPERTY_COUNT_MAX];
+  modular_server::Parameter parameters_[pulse_controller::constants::PARAMETER_COUNT_MAX];
+  modular_server::Function functions_[pulse_controller::constants::FUNCTION_COUNT_MAX];
+  modular_server::Callback callbacks_[pulse_controller::constants::CALLBACK_COUNT_MAX];
+
+  // Handlers
+
+};
+
+#endif
