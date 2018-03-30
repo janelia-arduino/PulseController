adf4c48ea279c67f01fd0bcd028fb75a83f5d292
Peter Polidoro <peterpolidoro@gmail.com>
2018-03-30 14:11:59 -0400

    Removed unnecessary header includes.

--- src/PulseController.h
+++ src/PulseController.h
@@ -7,23 +7,13 @@
 // ----------------------------------------------------------------------------
 #ifndef PULSE_CONTROLLER_H
 #define PULSE_CONTROLLER_H
-#include <Flash.h>
-#include <EEPROM.h>
-#include <Streaming.h>
 #include <ArduinoJson.h>
 #include <JsonSanitizer.h>
 #include <JsonStream.h>
 #include <Array.h>
 #include <Vector.h>
-#include <MemoryFree.h>
 #include <ConstantVariable.h>
-#include <SavedVariable.h>
 #include <Functor.h>
-#include <IndexedContainer.h>
-#include <FunctorCallbacks.h>
-#include <Watchdog.h>
-
-#include <ModularClient.h>
 
 #include <ModularServer.h>
 #include <ModularDeviceBase.h>
